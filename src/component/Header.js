2cb8178abd6912dd3a25b2313960c5ce0a76eaab
Rio Zakirahman <riozakirahman@gmail.com>
2022-12-22 16:26:43 +0700

    creating header component

---
+++ src/component/Header.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "../styles/header.css";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className="header">
+        <div className="container">
+          <div className="logo"></div>
+          <nav>
+            <ul className="menu">
+              <li className="menu__item">
+                <a href="" className="list__item">
+                  About Us
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="" className="list__item">
+                  Event
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="" className="list__item">
+                  Organization
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="" className="list__item">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
